1f50031abe742556b0a24fa355fe7ccf74dd8f54
bapt <bapt.cadilhac@gmail.com>
2017-09-11 23:18:36 +0200

    ajout de la camera

--- src/pages/medal/medal.ts
+++ src/pages/medal/medal.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-medal',
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class MedalPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  
+  takePicture() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
 }
