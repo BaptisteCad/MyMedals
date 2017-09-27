f2c7e6d39a29818378153264653de3215cd981d3
bapt <bapt.cadilhac@gmail.com>
2017-09-27 22:32:36 +0200

    starting of medals

---
+++ src/pages/addMedal/addMedal.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-add-medal',
+  templateUrl: 'addMedal.html'
+})
+export class AddMedalPage {
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
+
+  }
+
+  options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  
+  takePicture() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      var smallImage = <HTMLImageElement>document.getElementById('smallImage');
+      smallImage.style.display = 'block';
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
+}
--- src/pages/medal/medal.ts
+++ src/pages/medal/medal.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-medal',
@@ -8,24 +7,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class MedalPage {
 
-  constructor(public navCtrl: NavController, private camera: Camera) {
+  constructor(public navCtrl: NavController) {
 
-  }
-
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.DATA_URL,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
-  
-  takePicture() {
-    this.camera.getPicture(this.options).then((imageData) => {
-      var smallImage = <HTMLImageElement>document.getElementById('smallImage');
-      smallImage.style.display = 'block';
-      smallImage.src = "data:image/jpeg;base64," + imageData;
-     }, (err) => {
-      // Handle error
-     });
   }
 }
